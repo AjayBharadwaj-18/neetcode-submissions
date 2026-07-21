829ed1924c984a49a727a9405dfccd8190c4d4a7
Ajay A <85162250+AjayBharadwaj-18@users.noreply.github.com>
2026-07-21 08:10:41 +0530

    Add: validate-parentheses - submission-8

---
+++ Data Structures & Algorithms/validate-parentheses/submission-8.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()){
+            if (c == '(') stack.push(')');
+            else if (c == '{') stack.push('}');
+            else if (c == '[') stack.push(']');
+            else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+
+                char expectedClosingBracket = stack.pop();
+
+                if (expectedClosingBracket != c) {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
